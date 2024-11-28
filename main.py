bb8e228fe9d92135a3d146bfc51695906f9a0aba
ARANO8 <alanarnez51@gmail.com>
2024-11-28 11:13:16 -0400

    Agregamos las plantillas html del frontend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.config import Base, engine
 from app.routes.auth_routes import router as auth_router 
 from app.routes.usuario_routes import router as usuario_router
@@ -6,6 +7,9 @@ from app.routes.producto_routes import router as producto_router
 from app.routes.categoria_routes import router as categoria_router
 
 app = FastAPI()
+
+# Montar los archivos estaticos
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Incluir los routers de autenticación, usuarios, productos, y categorías
 app.include_router(auth_router, prefix="/api/v1", tags=["Autenticación"])
